0b126b48fc487a2a5c9cd2bbd3fa9257a77c7ab3
Abdellah Oulahyane <aitbaha48@gmail.com>
2022-09-07 19:27:06 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+import 'antd/dist/antd.min.css';
+import AdminHome from './compenents/admin/home/home';
+import TaskDone from './compenents/user/donetask/home';
+import UserHome from './compenents/user/home/home';
+import DetailsBar from './compenents/admin/detail/tabbar';
+import Detail from './compenents/admin/detail/detailsceen';
+
+
+
+
+
+
+
+function App() {
+  return(
+  <Detail/>
+  );
+}
+export default App;
